34b92e713c3f38662a3800e037766a5ccbeb5e5f
Vibhu Gupta <vik6026717961@gmail.com>
2023-08-26 21:25:34 +0530

    new ques

---
+++ Array/2D Array/Sprial_Matrix_Generate.java
@@ -0,0 +1,21 @@
+public class Sprial_Matrix_Generate {
+
+      static void printMatrix (int [] [] matrix ){
+                for(int i = 0 ; i < matrix.length; i++){
+                  for(int j = 0 ; j < matrix[i].length ; j++){
+                        System.out.print(matrix[i] [j]);
+                  }
+                  System.out.println();
+                }
+      }
+
+    static int [][] generate_Sprial_Matrix(int [] [] matrix , int row , int col){
+         int 
+
+      return 0;
+
+      }
+      public static void main(String[] args) {
+
+      }
+}
