369a16a1192112813682e15e7e8a73b27d9fa316
Vibhu Gupta <vik6026717961@gmail.com>
2023-08-28 23:47:49 +0530

    sprial problem Solve

--- Array/2D Array/Sprial_Matrix_Generate.java
+++ Array/2D Array/Sprial_Matrix_Generate.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Sprial_Matrix_Generate {
 
       static void printMatrix (int [] [] matrix ){
@@ -9,13 +11,66 @@ public class Sprial_Matrix_Generate {
                 }
       }
 
-    static int [][] generate_Sprial_Matrix(int [] [] matrix , int row , int col){
-         int 
 
-      return 0;
+
+    static int [][] generate_Sprial_Matrix(int n){
+
+      int [][] matrix = new int [n][n];
+
+      int topRow = 0 , bottomRow = n-1 , leftCol = 0 , rightCol=n-1;
+
+      int curr = 1;
+
+      while (curr <= n * n) {
+
+            for (int j = leftCol ; j <= rightCol &&  curr <= n * n  ; j++){
+                  matrix[topRow][j] =curr++;
+
+            }
+            topRow++;
+
+            //  rightCol  => TopROw to BottomRow
+
+            for(int i = topRow; i <= bottomRow &&  curr < n * n  ; i++){
+                  matrix[i][rightCol] =  curr++;
+
+
+            }
+            rightCol--;
+
+
+
+            //  BottomRow => RightCol to leftCol
+
+            for(int j = rightCol ; j >=  leftCol  &&  curr < n * n ; j--){
+                  matrix[bottomRow][j] =  curr++;
+
+            }
+            bottomRow--;
+
+
+
+            for(int i = bottomRow; i >= topRow &&  curr < n * n ; i--){
+                  matrix[i][leftCol] = curr++;
+
+            }
+            leftCol++;
+
 
       }
-      public static void main(String[] args) {
 
+      return matrix;
+
+      }
+
+      public static void main(String[] args) {
+               Scanner sc = new Scanner (System.in);
+
+               System.out.print(" \n Enter n : ");
+               int n = sc.nextInt();
+
+               int [][] matrix = generate_Sprial_Matrix(n);
+
+               printMatrix(matrix);
       }
 }
