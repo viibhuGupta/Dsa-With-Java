0e2780e6407ea0152598adc22c7fb22651b9ed98
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2023-12-23 00:42:34 +0530

    commit

--- Java/Array/2D Array/Two_Array_are_Equal.java
+++ Java/Array/2D Array/Two_Array_are_Equal.java
@@ -1,11 +1,14 @@
+import java.util.Arrays;
+
 public class Two_Array_are_Equal {
 
-      static long [] sortArray (long arr[]){
+
+      static int [] sortArray (int arr[]){
 
             for(int i = 0 ; i < arr.length ; i++){
                   for(int j = 0 ; j < arr.length ; j++){
-                        if (arr[j] < arr[i]) {
-                              long temp = arr[j];
+                        if (arr[j] > arr[i]) {
+                              int temp = arr[j];
                               arr[j] = arr[i];
                               arr[i] = temp;
                         }
@@ -15,29 +18,57 @@ public class Two_Array_are_Equal {
       }
 
 
-      static boolean arrayEqualCheck(long arr1[] , long arr2[] , int N){
+      static Boolean arrayEqualCheck(int arr1[] , int arr2[] , int N){
+
 
             sortArray(arr1);
             sortArray(arr2);
 
 
             for(int i=0; i< N; i++){
-                  if (arr1[i] == arr2[i]){ {
-                        return false;
-                  }
-                  
-            }
-            
-      }return true;
+                 
+                      if (arr1[i] != arr2[i]) {
+                  return false;
+                      }
+  
+            } return true;
+
+
       }
+
+    
 
       public static void main(String[] args) {
 
-          long  A[] = {1,2,5,4,0};
-          long   B[] = {2,4,5,0,1};
+           
+
+          int  A[] = {6,2,5,4,0};
+          int   B[] = {2,4,5,0,1 };
           int N = 5;
 
+            A = sortArray(A);
+            System.out.println("Sorted of A "+Arrays.toString(A));
+            sortArray(B);
+            B = sortArray(B);
+            System.out.println("Sorted of B "+Arrays.toString(B));
+    
             System.out.println(arrayEqualCheck(A, B, N));
 
+
+
+           
+
+
+            
+
+
+            
       }
-}
+
+          
+
+            
+
+
+      }
+
