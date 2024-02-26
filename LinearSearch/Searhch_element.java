3c06b452f2aebbdac7b44a5a90febc002a4251d4
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2024-02-26 12:38:42 +0530

    commit

---
+++ LinearSearch/Searhch_element.java
@@ -0,0 +1,14 @@
+package LinearSearch;
+
+public class Searhch_element {
+
+    static void linearSearch (int arr[] , int target ){
+        int ans = -1 ; // -1 because when we dont find any elements we return the -1
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,9};
+        int target ;
+    }
+    
+}
